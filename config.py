59427775686fbb67fb667f9c11967ff79672799c
guccisan <yamskymtb0903@gmail.com>
2022-10-01 00:19:21 +0900

    前処理+edaに着手

---
+++ config.py
@@ -0,0 +1,14 @@
+from tkinter import W
+
+data_path = "/mnt/sdb/NISHIKA_DATA/hate-speech-detection/"
+input_root = "./input/"
+output_root = "./output/"
+
+id_name = "id"
+label_name = "label"
+
+# mecab --
+dic_neologd = "/var/lib/mecab/dic/mecab-ipadic-neologd"
+
+# plot color --
+pal, color=["#016CC9", "#DEB078"], ["#8DBAE2", "#EDD3B3"]
