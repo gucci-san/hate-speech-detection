225833625ac7d90858927ae786d8ec68ac8432e1
guccisan <yamskymtb0903@gmail.com>
2022-10-09 15:04:08 +0900

    [add]cl-tohokuのlargeを使えるように

--- run.sh
+++ run.sh
@@ -5,14 +5,15 @@
 #python bert_run_train.py --run_id "roberta_baseline" --model_name "rinna/japanese-roberta-base" --epochs 10 --trial True
 #python bert_run_train.py --run_id "electra_baseline" --model_name "izumi-lab/electra-base-japanese-discriminator" --epochs 10 --trial True
 #python bert_run_train.py --run_id "roformer_baseline" --model_name "ganchengguang/Roformer-base-japanese" --epochs 10
+#python bert_run_train.py --run_id "bert_large" --model_name "cl-tohoku/bert-large-japanese" --epochs 10
 
 ## custom_header compare --
 #python bert_run_train.py --run_id "bert_conv" --model_custom_header "conv" --model_name "cl-tohoku/bert-base-japanese-whole-word-masking" --epochs 10
 #python bert_run_train.py --run_id "bert_lstm" --model_custom_header "lstm" --model_name "cl-tohoku/bert-base-japanese-whole-word-masking" --epochs 10
 #python bert_run_train.py --run_id "bert_cat4" --model_custom_header "concatenate-4" --model_name "cl-tohoku/bert-base-japanese-whole-word-masking" --epochs 10
 
-# trial --
-python bert_run_train.py --run_id "tmp" --folds 2 --trial True
+## # trial --
+## python bert_run_train.py --run_id "tmp" --model_name "cl-tohoku/bert-large-japanese" --folds 2 --trial True
 
 ## # prediction --
 #python bert_run_test.py --run_id "bert_baseline"
@@ -22,3 +23,4 @@ python bert_run_train.py --run_id "tmp" --folds 2 --trial True
 #python bert_run_test.py --run_id "bert_conv"
 #python bert_run_test.py --run_id "bert_lstm"
 #python bert_run_test.py --run_id "bert_cat4"
+python bert_run_test.py --run_id "bert_large"
